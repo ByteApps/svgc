b17f6320948ac02d160bcd5f613a2f7dd07bcc18
Salvador Guerrero <salvador.icc@gmail.com>
2014-09-21 10:24:20 -0700

    Initial Commit

---
+++ SVGC/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SVGC
+//
+//  Created by Salvador Guerrero on 9/21/14.
+//  Copyright (c) 2014 ByteApps. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
